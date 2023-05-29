8b6ecce15499e24eb15510e71a0bacb381fb27aa
Izdihar <ezdiharomerrrr@gmail.com>
2023-05-29 11:01:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function isPalindrome(str) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // Check if the clean string is equal to its reverse
+  return cleanStr === cleanStr.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  create the isPalindrome function
+  then create if statement
+  for each case, write a condition
+  take the condition from the test file
 */
 
 /*
